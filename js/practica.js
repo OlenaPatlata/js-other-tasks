4a185ee4f2ecb869160609edac5f4069972294a9
OlenaPatlata <94738131+OlenaPatlata@users.noreply.github.com>
2022-03-28 16:29:45 +0300

    Update practica.js

--- js/practica.js
+++ js/practica.js
@@ -358,30 +358,154 @@ function incrementString(strng) {
     return `${arr.join("")}` + `1`;
   }
 }
-
 // incrementString("foobar001");
 // incrementString("");
 // incrementString("foo009");
 // incrementString("foobar000");
 
-snail = function (array) {
-  // enjoy
-};
+// Практика 28.03.2022_______________________
+const users = [
+  {
+    id: "701b29c3-b35d-4cf1-a5f6-8b12b29a5081",
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    skills: ["ipsum", "lorem"],
+    gender: "male",
+    age: 37,
+  },
+  {
+    id: "7a3cbd18-57a1-4534-8e12-1caad921bda1",
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    skills: ["tempor", "mollit", "commodo", "veniam", "laborum"],
+    gender: "female",
+    age: 34,
+  },
+  {
+    id: "88beb2f3-e4c2-49f3-a0a0-ecf957a95af3",
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
+    skills: ["nulla", "anim", "proident", "ipsum", "elit"],
+    gender: "male",
+    age: 24,
+  },
+  {
+    id: "249b6175-5c30-44c6-b154-f120923736f5",
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
+    skills: ["adipisicing", "irure", "velit"],
+    gender: "female",
+    age: 21,
+  },
+  {
+    id: "334f8cb3-eb04-45e6-abf4-4935dd439b70",
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong"],
+    isActive: true,
+    balance: 3951,
+    skills: ["ex", "culpa", "nostrud"],
+    gender: "male",
+    age: 27,
+  },
+  {
+    id: "150b00fb-dd82-427d-9faf-2879ea87c695",
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: ["Jacklyn Lucas", "Linda Chapman"],
+    isActive: false,
+    balance: 1498,
+    skills: ["non", "amet", "ipsum"],
+    gender: "male",
+    age: 38,
+  },
+  {
+    id: "e1bf46ab-7168-491e-925e-f01e21394812",
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
+    skills: ["lorem", "veniam", "culpa"],
+    gender: "female",
+    age: 39,
+  },
+];
+const arrayAge = users
+  .filter((user) => user.age > 30)
+  .some((user) => user.isActive);
+// console.log("~ ~ arrayAge", arrayAge);
+const arrayFriends = users.flatMap((user) => user.friends);
+// console.log("~ ~ arrayFriends", arrayFriends);
+const balanceArray = users.find((user) => user.balance === 3793);
+// console.log("~ ~ balanceArray", balanceArray);
+const allBalance = users.reduce((acc, user) => {
+  return acc + user.balance;
+}, 0);
+// console.log("~ ~ allBalance", allBalance);
+const sortFriends = [...users].sort(
+  (a, b) => b.friends.length - a.friends.length
+);
+// console.log("~ ~ sortFriends", sortFriends);
 
-snail([[]]);
-snail([[1]]);
-snail([
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-]);
-snail([
-  [1, 2, 3, 4, 5],
-  [6, 7, 8, 9, 10],
-  [11, 12, 13, 14, 15],
-  [16, 17, 18, 19, 20],
-  [21, 22, 23, 24, 25],
-]);
+function finalObject(users) {
+  const object = {};
+  const keys = Object.keys(users[0]);
+  for (const key of keys) {
+    object[key] = [];
+  }
+  for (const key in object) {
+    for (const user of users) {
+      object[key].push(user[key]);
+    }
+  }
+  const newObject = {};
+  for (const key in object) {
+    newObject[key] = object[key]
+      .flat()
+      .filter((elem, index, array) => array.indexOf(elem) === index);
+  }
+  console.log(newObject);
+}
+finalObject(users);
+
+// snail = function (array) {
+//   // enjoy
+// };
+
+// snail([[]]);
+// snail([[1]]);
+// snail([
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9],
+// ]);
+// snail([
+//   [1, 2, 3, 4, 5],
+//   [6, 7, 8, 9, 10],
+//   [11, 12, 13, 14, 15],
+//   [16, 17, 18, 19, 20],
+//   [21, 22, 23, 24, 25],
+// ]);
 
 function zero(f) {
   let x = 0;
